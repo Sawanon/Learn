773b55f095b9fa18021fadb4768c82b048976446
Sawanon <sawanon01@hotmail.com>
2019-05-02 16:54:57 +0700

    02/05/62 //17.00

--- index.php
+++ index.php
@@ -2,9 +2,111 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Learn</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <style media="screen">
+      body{
+        color: #ffffff;
+      }
+      div{
+        border: solid 2px;
+        border-color: #000000;
+      }
+    </style>
   </head>
   <body>
-
+    <div class="container" style="background-color: #20e788;">
+      test
+      <div class="row" style="background-color: #e78620;">
+        test row
+        <div class="col-sm" style="background-color: #16869e;">
+          col 1
+        </div>
+        <div class="col-sm" style="background-color: #66a4b1;">
+          col 2
+        </div>
+        <div class="col-sm" style="background-color: #c0d5da;">
+          col 3
+        </div>
+        <div class="col-sm" style="background-color: #7ca854;">
+          col 4
+        </div>
+        test row
+      </div>
+      test
+    </div>
+    <br><br>
+    <div class="container" style="background-color: #f08625;">
+      container
+      <div class="row" style="background-color: #65cd14;">
+        Row
+        <div class="col" style="background-color: #25cbf0;">
+          col 1 of 3
+        </div>
+        <div class="col" style="background-color: #0e1898;">
+          col 2 of 3
+        </div>
+        <div class="col" style="background-color: #3c0f9d;">
+          col 3 of 3
+        </div>
+        Row
+      </div>
+      container
+    </div>
+    <br><br>
+    <div class="container" style="background-color: #f08625;">
+      container
+      <div class="row" style="background-color: #65cd14;">
+        row
+        <div class="col" style="background-color: #25cbf0;">
+          column
+        </div>
+        <div class="col" style="background-color: #25cbf0;">
+          column
+        </div>
+        <div class="w-100">
+          something
+        </div>
+        <div class="col" style="background-color: #25cbf0;">
+          column
+        </div>
+        <div class="col" style="background-color: #25cbf0;">
+          column
+        </div>
+        row
+      </div>
+      container
+    </div>
+    <br><br>
+    <div class="container" style="background-color: #338091;">
+      container
+      <div class="row">
+        row
+        <div class="col">
+          col 1 of 3
+        </div>
+        <div class="col-6">
+          col 2 of 3 / 6
+        </div>
+        <div class="col">
+          col 3 of 3
+        </div>
+        row
+      </div>
+      <div class="row">
+        row
+        <div class="col">
+          col 1 of 3
+        </div>
+        <div class="col-5">
+          col 2 of 3 / 5
+        </div>
+        <div class="col">
+          col 3 of 3
+        </div>
+        row
+      </div>
+      container
+    </div>
   </body>
 </html>
